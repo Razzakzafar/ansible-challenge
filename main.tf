0bf420d11ecabc40849f9f1aad3ec3b557d1d553
Razzakzafar <abdulrazzak1691@gmail.com>
2024-12-17 19:18:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,12 +3,13 @@ provider "aws" {
 }
 
 resource "aws_instance" "c8" {
-  ami           = "ami-01816d07b1128cd2d"  # Amazon Linux AMI ID
+  ami           = "ami-01816d07b1128cd2d"  # Use the Amazon Linux AMI ID
   instance_type = "t2.micro"
   key_name      = "ansible"
   tags = {
     Name = "c8.local"
   }
+  # Assign hostname
   user_data = <<-EOF
               #!/bin/bash
               hostnamectl set-hostname c8.local
@@ -16,18 +17,20 @@ resource "aws_instance" "c8" {
 }
 
 resource "aws_instance" "u21" {
-  ami           = "ami-0e2c8caa4b6378d8c"  # Ubuntu 21.04 AMI ID
+  ami           = "ami-0e2c8caa4b6378d8c"  # Use the Ubuntu 21.04 AMI ID
   instance_type = "t2.micro"
   key_name      = "ansible"
   tags = {
     Name = "u21.local"
   }
+  # Assign hostname
   user_data = <<-EOF
               #!/bin/bash
               hostnamectl set-hostname u21.local
               EOF
 }
 
+# Output instance details for Ansible dynamic inventory
 output "frontend" {
   value = aws_instance.c8.public_ip
 }
